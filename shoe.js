0595fc74c364f2c29dcc931930e08c31f697beb4
Josh Warner <joshwarner315@gmail.com>
2023-06-22 19:32:26 -0500

    shoesto

--- shoe.js
+++ shoe.js
@@ -11,25 +11,52 @@ function TheUsersName() {
     return document.write('Whats Good !!!! ' + usersName);
 }
 
-function shoeJordanNumber() {
-    let shoeJordanNumber = prompt('What is your favorite pair of Jordans ');
+
+function typesJordanRetro() {
+   let answer;
+
+    while(answer != 35) {
+        answer = prompt('How many Jordan editions are there?');
+           if (answer !=35) {
+            alert('Try Again!!')
+           } else {
+            alert("Thats Right!!!!");
+           }
+        }
+    }    
+    
+
+
+    function usersFavJordan() {
+    let shoe = prompt('Whats Your FavJordan between 1-13');
+
     let message;
-    if(shoeJordanNumber == '') {
-        shoeJordanNumber();
+    if(shoe == '') {
+        usersFavJordan();
     }
-    else if (shoeJordanNumber < 13) {
-        message = 'You got that heat!!! ';
+    else if (shoe  < 4) {
+        message = 'Cant beat the OGs!!!';
     }
-    else if (shoeJordanNumber > 13) {
-        message = 'I see ya !! '; 
+    else if (shoe <= 13) {
+        message = 'Love these!!!'; 
     }
-    else if (shoeJordanNumber <= 8) {
+    else if (shoe >= 8) {
         message = 'These are the ones !!! ';
     }
     else {
-        message = 'All Styles of shoes are accepted here !!! ';
+        message = 'That is not a valid entry. Only use numbers';
         }
         console.log(message);
         return document.write(message);
-}
+    }
 
+
+function siteRating () {
+    let output = '';
+    let rating = prompt('Rate the site on scale of 1-5');
+    for(let i = 0; i < rating; i++){
+        output =+ "<img class='shoerate'https://images.unsplash.com/photo-1617813254792-fdff65b515a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80' />"
+    }
+    return document.write(output);
+
+}
